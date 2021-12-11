6f7b6087e80d96189e30279ae85ddb66aaa216f4
Takashi Mori <kanetugu2015@gmail.com>
2021-12-11 18:50:23 +0900

    add hello world

---
+++ project/src/main/java/com/example/ExampleController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExampleController {
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello World");
+        return "hello";
+    }
+
+}
